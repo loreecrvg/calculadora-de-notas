bae69934daf994cabdb9975fdd8756374fc78b3f
Lorena Narciso <lorenanarciso97@gmail.com>
2021-09-17 10:22:05 -0300

    confirmar

---
+++ script.js
@@ -0,0 +1,37 @@
+function calcularMedia() {
+    var nome = prompt("Por favor, insira seu nome");
+    var notaFinal = "Sua nota final é:";
+    var notaDoPrimeiroBimestre = parseInt(
+      prompt("Por favor, insira a nota do primeiro bimestre")
+    );
+    var notaDoSegundoBimestre = parseInt(
+      prompt("Por favor, insira a nota do segundo bimestre")
+    );
+    var notaDoTerceiroBimestre = parseInt(
+      prompt("Por favor, insira a nota do terceiro bimestre")
+    );
+    var notaDoQuartoBimestre = parseInt(
+      prompt("Por favor, insira a nota do quarto bimestre")
+    );
+    var notaFinal =
+      (notaDoPrimeiroBimestre +
+        notaDoSegundoBimestre +
+        notaDoTerceiroBimestre +
+        notaDoQuartoBimestre) /
+      4;
+    var notaFixada = notaFinal.toFixed(2);
+    console.log(notaFinal);
+    var textoNota = document.getElementById("textoNota");
+  
+    textoNota.innerHTML = notaFixada;
+  
+    var textoAprovacao = document.getElementById("textoAprovacao");
+    var aprovado = "Aprovado";
+  
+    if (notaFinal < 5) {
+      aprovado = "Reprovado";
+    }
+  
+    textoAprovacao.innerHTML = aprovado;
+  }
+  
